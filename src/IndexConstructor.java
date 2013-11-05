63cc7e85b644f62347aa25ba36d34ba3e8507eb7
Soheil Behnezhad <soheil.behnezhad@gmail.com>
2013-11-06 00:29:44 +0330

    Initialize project and design basic API of tokenizer/index constructor

---
+++ src/IndexConstructor.java
@@ -0,0 +1,29 @@
+public class IndexConstructor {
+
+	// TODO
+	public void index(String path) {
+		/*
+		 * Index every file in the given @path
+		 */
+	}
+
+	// TODO
+	public void printDictionary() {
+
+	}
+
+	// TODO
+	public void printPostingList(String term) {
+
+	}
+
+	// TODO
+	public Boolean save(String path) {
+		return false;
+	}
+
+	// TODO
+	public Boolean load(String path) {
+		return false;
+	}
+}
---
+++ src/Tokenizer.java
@@ -0,0 +1,17 @@
+public class Tokenizer {
+	private String document;
+
+	public Tokenizer(String document) {
+		this.document = document;
+	}
+
+	public Boolean hasNext() {
+		// TODO
+		return false;
+	}
+
+	public String next() {
+		// TODO
+		return "";
+	}
+}
