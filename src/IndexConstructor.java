192e6e94f61fc1795bf0b13aec143bd46d09ff1b
MaHsa <mahsa@MyVaio.(none)>
2013-11-06 22:10:29 +0330

    Implement tokenizer

--- src/IndexConstructor.java
+++ src/IndexConstructor.java
@@ -1,12 +1,58 @@
-public class IndexConstructor {
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.File;
+import java.io.PrintWriter;
+import java.util.ArrayList;
 
-	// TODO
-	public void index(String path) {
-		/*
-		 * Index every file in the given @path
-		 */
+import java.util.Collections;
+
+
+public class IndexConstructor {
+	
+	
+	
+	public static void main(String args[]) throws IOException{
+		IndexConstructor indexer= new IndexConstructor();
+		indexer.index("/home/mahsa/workspace/dangerzone/Data/Time Test Collection/Docs");
 	}
 
+	// TODO
+	public void index(String path) throws IOException {
+		
+		File folder = new File(path);
+		File[] listOfFiles = folder.listFiles();
+		ArrayList<String> fileNames = new ArrayList<String>();
+		
+		for (File file : listOfFiles) {
+		    if (file.isFile()) {
+		        fileNames.add(file.getName());
+		    }
+		}
+		
+		for(int i=0; i<fileNames.size(); i++){
+			int c=0;
+			String input="", output="";
+			String inputPath=path+ "/" +fileNames.get(i);
+			String outputPath="/home/mahsa/workspace/dangerzone/Data/Time Test Collection/Tokenized/"+fileNames.get(i);
+			FileWriter writer = new FileWriter(outputPath);
+			BufferedReader reader = new BufferedReader(new FileReader(inputPath));
+			
+			String line = null;
+			while ((line = reader.readLine()) != null) {
+			    input+=line+'\n';
+			}
+			reader.close();
+			Tokenizer tokenizer= new Tokenizer(input);
+			while(tokenizer.hasNext()){
+				output+=tokenizer.next()+'\n';
+			}
+			writer.write(output);
+			writer.close();
+		}
+	}
+	
 	// TODO
 	public void printDictionary() {
 
